e98ef6ebea892ad196ad9c3914766ab4c5afe7c7
Marina Nakagawa <mari10172001@yahoo.co.jp>
2018-05-27 23:13:10 +0900

    とりあえず途中まで

--- RxSwift_for_code/ViewController.swift
+++ RxSwift_for_code/ViewController.swift
@@ -11,70 +11,137 @@ import RxSwift
 import RxCocoa
 import SnapKit
 
+fileprivate let minimalUsernameLength = 5
+fileprivate let minimalPasswordLength = 5
+
 class ViewController: UIViewController {
     var disposeBag = DisposeBag()
 
-    var number1: UITextField = {
-        let textField = UITextField()
-        textField.borderStyle = .roundedRect
-        return textField
-    }()
-    var number2: UITextField = {
-        let textField = UITextField()
-        textField.borderStyle = .roundedRect
-        return textField
-    }()
-    var number3: UITextField = {
-        let textField = UITextField()
-        textField.borderStyle = .roundedRect
-        return textField
-    }()
-
-    var label: UILabel = {
+    var userName: UILabel = {
         let label = UILabel()
+        label.text = "userName"
         return label
     }()
 
+    var userNameField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+
+    var userNameValidLable: UILabel = {
+        let label = UILabel()
+        label.text = "userName"
+        return label
+    }()
+
+    var password: UILabel = {
+        let label = UILabel()
+        label.text = "password"
+        return label
+    }()
+
+    var passwordField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+
+    var passwordValidLable: UILabel = {
+        let label = UILabel()
+        label.text = "password"
+        return label
+    }()
+
+    var button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor.blue
+        button.setTitle("テスト", for: .normal)
+        return button
+    }()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(self.number1)
-        self.number1.snp.makeConstraints { (make) -> Void in
-            make.width.equalTo(200)
+        self.initializeUILayout()
+        userNameValidLable.text = "ユーザー名は\(minimalUsernameLength) 文字以上"
+        passwordValidLable.text = "パスワードは\(minimalPasswordLength) 文字以上"
+
+        let userNameValid = userNameField.rx.text.orEmpty
+            .map{$0.count >= minimalUsernameLength}
+            //このマップがない場合は、バインディングごとに1回実行され、rxはデフォルトではステートレスです
+            .share(replay: 1)
+
+        let passwordValid = passwordField.rx.text.orEmpty
+            .map{ $0.count >= minimalPasswordLength}
+            .share(replay: 1)
+
+        let everythingValid = Observable.combineLatest(userNameValid, passwordValid){ $0 && $1 }
+            .share(replay: 1)
+
+        userNameValid.bind(to: passwordField.rx.isEnabled).disposed(by:disposeBag)
+
+        userNameValid.bind(to: userNameField.rx.isHidden).disposed(by:disposeBag)
+
+        
+
+
+
+
+    }
+
+    func initializeUILayout() {
+        self.view.addSubview(self.userName)
+        self.userName.snp.makeConstraints { (make) -> Void in
+            make.width.equalTo(300)
             make.height.equalTo(50)
-            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(100)
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(10)
             make.centerX.equalTo(self.view)
         }
-        self.view.addSubview(self.number2)
-        self.number2.snp.makeConstraints { (make) -> Void in
-            make.width.equalTo(200)
+        self.view.addSubview(self.userNameField)
+        self.userNameField.snp.makeConstraints { (make) -> Void in
+            make.width.equalTo(300)
             make.height.equalTo(50)
-            make.top.equalTo(self.number1.snp.bottom)
+            make.top.equalTo(self.userName.snp.bottom)
             make.centerX.equalTo(self.view)
         }
-        self.view.addSubview(self.number3)
-        self.number3.snp.makeConstraints { (make) -> Void in
-            make.width.equalTo(200)
+        self.view.addSubview(self.userNameValidLable)
+        self.userNameValidLable.snp.makeConstraints { (make) -> Void in
+            make.width.equalTo(300)
             make.height.equalTo(50)
-            make.top.equalTo(self.number2.snp.bottom)
-            make.centerX.equalTo(self.view)
-        }
-        self.view.addSubview(self.label)
-        self.label.snp.makeConstraints { (make) -> Void in
-            make.width.equalTo(200)
-            make.height.equalTo(50)
-            make.top.equalTo(self.number3.snp.bottom)
+            make.top.equalTo(self.userNameField.snp.bottom)
             make.centerX.equalTo(self.view)
         }
 
-       //観測可能なシーケンスのいずれかが要素を生成するたびに、セレクタ関数を使用して、指定された観測可能なシーケンスを1つの観測可能なシーケンスにマージします。
-    Observable.combineLatest(number1.rx.text.orEmpty,number2.rx.text.orEmpty,number3.rx.text.orEmpty) { textValue1, textValue2, textValue3 -> Int in
-            return (Int(textValue1) ?? 0) + (Int(textValue2) ?? 0) + (Int(textValue3) ?? 0)
+        self.view.addSubview(self.password)
+        self.password.snp.makeConstraints { (make) -> Void in
+            make.width.equalTo(300)
+            make.height.equalTo(50)
+            make.top.equalTo(self.userNameValidLable.snp.bottom)
+            make.centerX.equalTo(self.view)
         }
-        //入ってきた値をStringに変換する関数
-        .map { $0.description}
-        //新しいサブスクリプションを作成し、オブザーバに要素を送信します。
-        .bind(to: label.rx.text)
-        .disposed(by: disposeBag)
+        self.view.addSubview(self.passwordField)
+        self.passwordField.snp.makeConstraints { (make) -> Void in
+            make.width.equalTo(300)
+            make.height.equalTo(50)
+            make.top.equalTo(self.password.snp.bottom)
+            make.centerX.equalTo(self.view)
+        }
+        self.view.addSubview(self.passwordValidLable)
+        self.passwordValidLable.snp.makeConstraints { (make) -> Void in
+            make.width.equalTo(300)
+            make.height.equalTo(50)
+            make.top.equalTo(self.passwordField.snp.bottom)
+            make.centerX.equalTo(self.view)
+        }
+
+        self.view.addSubview(self.button)
+        self.button.snp.makeConstraints { (make) -> Void in
+            make.width.equalTo(300)
+            make.height.equalTo(50)
+            make.top.equalTo(self.passwordValidLable.snp.bottom)
+            make.centerX.equalTo(self.view)
+        }
     }
 
     override func didReceiveMemoryWarning() {
